95b527ef586bf6332de9a010b8811c84d3f10f9c
Jacob Trier Christensen <jacorpski@gmail.com>
2014-10-22 11:00:18 +0200

    Added some testing, so we can see the basic of the pipeline architecture

---
+++ TestTheCracking/Buffer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTheCracking
+{
+    public class Buffer
+    {
+        private ConcurrentQueue<int> sharedBuffer = new ConcurrentQueue<int>();
+
+
+        public void Put(int value)
+        {
+            sharedBuffer.Enqueue(value);
+        }
+
+        public int Take()
+        {
+            int value;
+            int outValue = -1;
+
+            if (!IsEmpty())
+            {
+                if (sharedBuffer.TryDequeue(out value))
+                {
+                    outValue = value;
+                }
+            }
+
+            return outValue;
+        }
+
+        public bool IsEmpty()
+        {
+            return getCount() <= 0;
+        }
+
+        public int getCount()
+        {
+            return sharedBuffer.Count;
+        }
+    }
+}
---
+++ TestTheCracking/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTheCracking
+{
+    class Program
+    {
+        static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            Buffer sharedBuffer1 = new Buffer();
+            Buffer sharedBuffer2 = new Buffer();
+
+            TaskFactory f = new TaskFactory();
+
+            var stage1 = f.StartNew(() => Stage1Buffer(sharedBuffer1));
+            var stage2 = f.StartNew(() => Stage2Buffer(sharedBuffer1, sharedBuffer2));
+
+            Task.WaitAll(stage1, stage2);
+
+            int count = sharedBuffer2.getCount();
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(sharedBuffer2.Take());
+            }
+        }
+
+        static void Stage1Buffer(Buffer sharedBufferIn)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int randomNumber = random.Next();
+
+                Console.WriteLine("Stage 1: " + randomNumber);
+
+                sharedBufferIn.Put(randomNumber);
+            }
+        }
+
+        static void Stage2Buffer(Buffer sharedBufferOut, Buffer sharedBufferIn)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                int buffer = sharedBufferOut.Take();
+
+                if (buffer != -1)
+                {
+                    buffer = buffer + 10;
+
+                    Console.WriteLine("Stage 2: " + buffer);
+
+                    sharedBufferIn.Put(buffer);
+                }
+                else
+                {
+                    Console.WriteLine("Stage 2: Buffer empty");
+                }
+            }
+        }
+    }
+}
