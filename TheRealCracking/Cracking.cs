f2d25dacf751851239de6f35a7411070657b9aca
Jacob Trier Christensen <jacorpski@gmail.com>
2014-10-27 11:05:39 +0100

    Added comments

--- TheRealCracking/Cracking.cs
+++ TheRealCracking/Cracking.cs
@@ -11,35 +11,44 @@ namespace TheRealCracking
 {
     public class Cracking
     {
+        // Contain the hash algorithm we choose to use
         private readonly HashAlgorithm _messageDigest;
 
+        // All the buffers we need
         private Buffer BufferOfStrings          = new Buffer(10);
         private Buffer BufferOfEncryptedStrings = new Buffer(10);
         private Buffer BufferOfMatches          = new Buffer(10);
 
+        // Hold the dictionary file
         private FileStream dictionaryFile;
 
+        // Constructor - set the hash algorithm to use
         public Cracking()
         {
             _messageDigest = new SHA1CryptoServiceProvider();
         }
 
-
+        // Run the cracking program
         public void RunCracking()
         {
+            // Read the dictionary file, and put it into a variable
             ReadDictionary();
 
             TaskFactory f = new TaskFactory();
 
+            // The stages the program are going to use
             var ReadAllStringsStage = f.StartNew(() => ReadAllStrings(dictionaryFile, BufferOfStrings));
 
+            // We need to wait on all stages before we can continue
             Task.WaitAll(ReadAllStringsStage);
 
+            // Close the dictionary file
             dictionaryFile.Close();
         }
 
         private void ReadDictionary()
         {
+            // Open and read the dictionary
             dictionaryFile = new FileStream("webster-dictionary.txt", FileMode.Open, FileAccess.Read);
         }
 
@@ -47,8 +56,10 @@ namespace TheRealCracking
         {
             using (StreamReader temp = new StreamReader(dictionary))
             {
+                // If we hadn't hit the end of the file, then continue
                 while (!temp.EndOfStream)
                 {
+                    // Temporary place for the string
                     String put = temp.ReadLine();
 
                     if (sharedBuffer.Put(put))
